4c519e46c998d1ad56fd67186310265020c33091
Angelo Mafilas <145873573+AngeloMafilas@users.noreply.github.com>
2026-01-22 19:56:49 +0400

    Use environment variable for Clerk domain

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,8 +1,8 @@
 export default {
   providers: [
     {
-      domain: "https://driving-cub-26.clerk.accounts.dev",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN || "https://driving-cub-26.clerk.accounts.dev",
       applicationID: "convex",
     },
-  ]
+  ],
 };
