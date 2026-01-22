0ca87908d6362c603513865e55b7eca8155df90c
Angelo Mafilas <145873573+AngeloMafilas@users.noreply.github.com>
2026-01-22 19:28:33 +0400

    updated auth configurations

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,8 +1,8 @@
 export default {
-	providers: [
-		{
-			domain: "https://dear-sole-2.clerk.accounts.dev/",
-			applicationID: "convex",
-		},
-	],
+  providers: [
+    {
+      domain: "https://driving-cub-26.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
 };
